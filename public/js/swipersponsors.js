f39a29fa838f9615bd9e45c3620c010796354493
Didier Crespo <dj.crespo.castilla@gmail.com>
2021-08-12 22:22:35 -0500

    New changes

--- public/js/swipersponsors.js
+++ public/js/swipersponsors.js
@@ -2,7 +2,7 @@ const dataSponsors = [
     {
         name: 'IISE',
         img: './images/sponsors/iise.png',
-        link: 'https://developer.huawei.com/consumer/en/',
+        link: 'https://www.facebook.com/IISECAP783',
     },
     {
         name: 'CESA ITM',
@@ -10,44 +10,14 @@ const dataSponsors = [
         link: 'https://www.facebook.com/CESAITMID',
     },
     {
-        name: 'Stackly Code',
-        img: './images/sponsors/sponsor-stacklycode.webp',
-        link: 'https://stacklycode.com/',
+        name: 'Rescate Zarigüeyal',
+        img: './images/sponsors/rescate_zarigueyal.png',
+        link: 'http://www.rescatezarigueyal.org/',
     },
     {
-        name: 'José Dimas Luján',
-        img: './images/sponsors/sponsor-jose_dimas_lujan.webp',
-        link: 'https://www.youtube.com/channel/UC17hjKubJGdyPbWI5jvyiVg',
-    },
-    {
-        name: 'Domini Code',
-        img: './images/sponsors/sponsor-dominicode.webp',
-        link: 'https://www.youtube.com/channel/UC3QuZuJr2_EOUak8bWUd74A',
-    },
-    {
-        name: 'TekkiTV',
-        img: './images/sponsors/sponsor-tekkitv.webp',
-        link: 'https://tekki.tv/',
-    },
-    {
-        name: 'CodelyTV',
-        img: './images/sponsors/sponsor-codelytv.webp',
-        link: 'https://codely.tv/pro/comfeco',
-    },
-    {
-        name: 'LatamDev',
-        img: './images/sponsors/sponsor-latamdev.webp',
-        link: 'http://latamdev.co/',
-    },
-    {
-        name: 'Códigofacilito',
-        img: './images/sponsors/sponsor-codigofacilito.webp',
-        link: 'https://codigofacilito.com/',
-    },
-    {
-        name: 'egghead',
-        img: './images/sponsors/sponsor-egghead.webp',
-        link: 'https://egghead.io/',
+        name: 'Autmix',
+        img: './images/sponsors/autmix.png',
+        link: 'https://autmix.com/',
     },
 ]
 
